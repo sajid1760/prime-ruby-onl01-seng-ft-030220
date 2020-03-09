4a72b6e59e7184c2098256414403c3639509fc96
Sajid Anjum <sajid1760@gmail.com>
2020-03-09 06:47:21 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(num)
+a = 2
+iter = num - 1
+boolers = TRUE
+if num < 2 then
+  boolers = FALSE
+end
+if num == 2 then
+  boolers = TRUE
+end
+while a < iter do 
+  if num % a == 0 then
+    boolers = FALSE
+  end
+  a += 1
+end
+  boolers
+end
+
+##puts prime?(101)
