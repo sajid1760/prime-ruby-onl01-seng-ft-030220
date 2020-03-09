27d524329c26c348ad026a76cfbcfd1ba8533abf
Sajid Anjum <sajid1760@gmail.com>
2020-03-09 06:44:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(num)
+a = 2
+iter = num - 1
+boolers = TRUE
+if num < 2 then
+  boolers = FALSE
+end
+if num == 2 then
+  boolers = TRUE
+end
+while a < iter do 
+  if num % a == 0 then
+    boolers = FALSE
+    a = a + 1
+  end
+end
+  boolers
+end
